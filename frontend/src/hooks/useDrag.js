58bf4cf573bb595e38453840e885c62fb4fc2cfd
Toothpaste <kwanho.kh.chan@gmail.com>
2026-04-10 16:21:53 +1000

    refactor: created useDrag hook and updated menu styling and stats component implementation

---
+++ frontend/src/hooks/useDrag.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+// hr = windowref
+// tr = targetRef
+
+export const useDrag = (hr, tr = hr) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [grabbing, setGrabbing] = useState(false)
+  const [offSet, setOffSet] = useState({ xo: 0, yo: 0 })
+
+  // Attach the mousedown listener to the element itself
+  useEffect(() => {
+    const node = hr.current;
+    const parent = tr.current;
+    if (!node || !parent) {
+      console.error("useDrag.js: ref object passed is null");
+      return;
+    }
+    const handleMouseDown = (e) => {
+      setGrabbing(true)
+      const { left, top } = parent.getBoundingClientRect();
+      const newOffSet = {
+        xo: e.clientX - left,
+        yo: e.clientY - top
+      };
+      setOffSet(newOffSet);
+    }
+
+    node.addEventListener('mousedown', handleMouseDown)
+
+    return () => {
+      node.removeEventListener('mousedown', handleMouseDown)
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!grabbing) return;
+
+    const handleMouseUp = () => {
+      setGrabbing(false)
+    }
+
+    const handleMouseMove = (e) => {
+      const newPos = {
+        x: e.clientX - offSet.xo,
+        y: e.clientY - offSet.yo
+      }
+      setPosition(newPos);
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+    window.addEventListener('mouseup', handleMouseUp)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('mouseup', handleMouseUp)
+    }
+  }, [grabbing])
+
+  return { position }
+}
--- frontend/src/pages/stats/stats.jsx
+++ frontend/src/pages/stats/stats.jsx
@@ -1,11 +1,33 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useRef } from 'react';
 import { SessionHistory } from '../../components/stats/history';
+import { useDrag } from '../../hooks/useDrag';
 
 export const Stats = () => {
   const [openHistory, setOpenHistory] = useState(false);
+  const elementRef = useRef(null);
+  const { position } = useDrag(elementRef)
+
+
 
   return (
     <>
+      <div
+        ref={elementRef}
+        style={{
+          width: '100px',
+          height: '100px',
+          color: 'white',
+          backgroundColor: 'red',
+          position: 'fixed',
+          top: position.y,
+          left: position.x,
+        }}
+      >
+        drag me
+      </div>
+
+
+
       <button className='history-button' onClick={() => setOpenHistory(true)}>i</button>
       {openHistory ?
         <div className='modal-bg'>
